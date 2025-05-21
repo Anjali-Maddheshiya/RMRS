a005b01fdd594250d74b973a6d11801325ac675f
Anjali Maddheshiya <mgoldi147@gmail.com>
2025-05-22 02:16:59 +0530

    Initial commit - Uploaded core files of Rapid Maintenance and Response System (RMRS)
    This is a Spring Boot MVC based project for managing maintenance and repair requests in HAL(Hindustan Aeronautics Limited) like system.  
    It includes modules for user registration, complaint tracking, and admin response handling.  
    Technologies used: Java, Spring Boot, MySQL, HTML, CSS (Thymeleaf).

---
+++ RmrsApplication.java
@@ -0,0 +1,13 @@
+package com.project.RMRS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmrsApplication.class, args);
+	}
+
+}
